8ff13fde04ca2dac70b1bba11ebdaf299efec99c
MMMZZZZ <m.zuidberg@icloud.com>
2020-05-08 20:17:32 +0200

    Created Oneshot Timer Class

---
+++ Tiva_Interrupter_Source/Oneshot.cpp
@@ -0,0 +1,98 @@
+/*
+ * Oneshot.cpp
+ *
+ *  Created on: 08.05.2020
+ *      Author: Max
+ */
+
+#include <Oneshot.h>
+
+
+Oneshot::Oneshot()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Oneshot::~Oneshot()
+{
+    // TODO Auto-generated destructor stub
+}
+
+void Oneshot::init(System *sys, uint32_t timerNum, void (*ISR)(void))
+{
+    oneshotSys = sys;
+    oneshotTimerNum = timerNum;
+
+    // Timer base stored separately for better readability (used very often).
+    oneshotTimerBase = ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_TIMER_BASE];
+
+    SysCtlPeripheralEnable(ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_TIMER_SYSCTL_PERIPH]);
+    SysCtlPeripheralEnable(ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PORT_SYSCTL_PERIPH]);
+    SysCtlDelay(2);
+
+    // Timer A generates the ontime, timer B assures enough offtime until the next pulse
+    TimerConfigure(oneshotTimerBase, ONESHOT_TIMER_CONFIG);
+    TimerClockSourceSet(oneshotTimerBase, TIMER_CLOCK_PIOSC);
+
+    // Invert output which means enable on match, clear on reload.
+    TimerControlLevel(oneshotTimerBase, TIMER_A, true);
+
+    // ISR stops the timer (artificial oneshot mode), and allows to set new values.
+    TimerControlEvent(oneshotTimerBase, TIMER_A, TIMER_EVENT_NEG_EDGE);
+    TimerIntRegister(oneshotTimerBase, TIMER_A, ISR);
+    TimerIntEnable(oneshotTimerBase, TIMER_CAPA_EVENT);
+
+    // Make sure output is low before enabling the GPIO Output
+    TimerPrescaleSet(oneshotTimerBase, TIMER_A, 0);
+    TimerLoadSet(oneshotTimerBase, TIMER_A, 65535);
+    TimerPrescaleMatchSet(oneshotTimerBase, TIMER_A, 0xff);
+    TimerMatchSet(oneshotTimerBase, TIMER_A, 0xff);
+    TimerEnable(oneshotTimerBase, TIMER_A);
+    // while (!oneshotReady);
+    while (GPIOPinRead(ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PORT_BASE], ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PIN]));
+    TimerDisable(oneshotTimerBase, TIMER_A);
+
+    GPIOPinConfigure(ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PIN_CONFIG]);
+    GPIOPinTypeTimer(ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PORT_BASE], ONESHOT_MAPPING[oneshotTimerNum][ONESHOT_PIN]);
+
+    oneshotReady = true;
+}
+
+void Oneshot::rearmISR()
+{
+    TimerIntClear(oneshotTimerBase, TIMER_CAPA_EVENT);
+    TimerDisable(oneshotTimerBase, TIMER_A);
+    oneshotReady = true;
+}
+
+void Oneshot::setMaxOntimeUS(uint32_t maxOntimeUS)
+{
+    oneshotMaxOnValue = maxOntimeUS * (oneshotSys->getPIOSCFreq() / 1000000);
+}
+
+void Oneshot::setMinOfftimeUS(uint32_t minOfftimeUS)
+{
+    oneshotMinOffValue = minOfftimeUS * (oneshotSys->getPIOSCFreq() / 1000000);
+}
+
+void Oneshot::shot(uint32_t ontimeUS)
+{
+    uint32_t matchValue = ontimeUS * (oneshotSys->getPIOSCFreq() / 1000000) * oneshotReady;
+    if (matchValue)
+    {
+        if (matchValue > oneshotMaxOnValue)
+        {
+            matchValue = oneshotMaxOnValue;
+        }
+        TimerPrescaleMatchSet(oneshotTimerBase, TIMER_A, ((matchValue & 0xff0000) >> 16));
+        TimerMatchSet(oneshotTimerBase, TIMER_A, (matchValue & 0xffff));
+
+        // Ensure min offtime after the ontime
+        matchValue += oneshotMinOffValue;
+        TimerPrescaleSet(oneshotTimerBase, TIMER_A, ((matchValue & 0xff0000) >> 16));
+        TimerLoadSet(oneshotTimerBase, TIMER_A, (matchValue & 0xffff));
+        oneshotReady = false;
+        TimerEnable(oneshotTimerBase, TIMER_A);
+    }
+}
---
+++ Tiva_Interrupter_Source/Oneshot.h
@@ -0,0 +1,62 @@
+/*
+ * Oneshot.h
+ *
+ *  Created on: 08.05.2020
+ *      Author: Max
+ */
+
+#ifndef ONESHOT_H_
+#define ONESHOT_H_
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"              // Macros defining the memory map of the Tiva C Series device. This includes defines such as peripheral base address locations such as GPIO_PORTF_BASE.
+#include "inc/hw_types.h"               // Defines common types and macros.
+#include "inc/hw_gpio.h"                // Defines and Macros for GPIO hardware.
+#include "inc/hw_timer.h"               // Defines and macros used when accessing the timer.
+#include "inc/hw_ints.h"
+#include "driverlib/pin_map.h"          // Mapping of peripherals to pins for all parts.
+#include "driverlib/sysctl.h"           // Defines and macros for System Control API of DriverLib. This includes API functions such as SysCtlClockSet.
+#include "driverlib/gpio.h"             // Defines and macros for GPIO API of DriverLib. This includes API functions such as GPIOPinWrite.
+#include "driverlib/interrupt.h"        // Defines and macros for NVIC Controller (Interrupt) API of driverLib. This includes API functions such as IntEnable and IntPrioritySet.
+#include "driverlib/timer.h"            // Defines and macros for timer API of driverLib.
+#include "System.h"
+
+
+class Oneshot
+{
+public:
+    Oneshot();
+    virtual ~Oneshot();
+    void init(System *sys, uint32_t timer, void (*ISR)(void));
+    void setMaxOntimeUS(uint32_t maxOntimeUS);
+    void setMinOfftimeUS(uint32_t minOfftimeUS);
+    void shot(uint32_t ontimeUS);
+    void rearmISR();
+    bool ready();
+private:
+    static constexpr uint32_t ONESHOT_TIMER_SYSCTL_PERIPH = 0;
+    static constexpr uint32_t ONESHOT_TIMER_BASE          = 1;
+    static constexpr uint32_t ONESHOT_PORT_SYSCTL_PERIPH  = 2;
+    static constexpr uint32_t ONESHOT_PORT_BASE           = 3;
+    static constexpr uint32_t ONESHOT_PIN                 = 4;
+    static constexpr uint32_t ONESHOT_PIN_CONFIG          = 5;
+    const uint32_t ONESHOT_MAPPING[6][6] =
+        {{SYSCTL_PERIPH_TIMER0, TIMER0_BASE, SYSCTL_PERIPH_GPIOD, GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_PD0_T0CCP0},
+         {SYSCTL_PERIPH_TIMER1, TIMER1_BASE, SYSCTL_PERIPH_GPIOD, GPIO_PORTD_BASE, GPIO_PIN_2, GPIO_PD2_T1CCP0},
+         {SYSCTL_PERIPH_TIMER2, TIMER2_BASE, SYSCTL_PERIPH_GPIOM, GPIO_PORTM_BASE, GPIO_PIN_0, GPIO_PM0_T2CCP0},
+         {SYSCTL_PERIPH_TIMER3, TIMER3_BASE, SYSCTL_PERIPH_GPIOM, GPIO_PORTM_BASE, GPIO_PIN_2, GPIO_PM2_T3CCP0},
+         {SYSCTL_PERIPH_TIMER4, TIMER4_BASE, SYSCTL_PERIPH_GPIOM, GPIO_PORTM_BASE, GPIO_PIN_4, GPIO_PM4_T4CCP0},
+         {SYSCTL_PERIPH_TIMER5, TIMER5_BASE, SYSCTL_PERIPH_GPIOM, GPIO_PORTM_BASE, GPIO_PIN_6, GPIO_PM6_T5CCP0}
+    };
+    static constexpr uint32_t ONESHOT_TIMER_CONFIG = TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM;
+
+    System* oneshotSys;
+    uint32_t oneshotMinOffValue = 1600;
+    uint32_t oneshotMaxOnValue = 1600;
+    uint32_t oneshotTimerNum = 0, oneshotTimerBase = 0;
+    volatile bool oneshotReady = false;
+};
+
+#endif /* ONESHOT_H_ */
