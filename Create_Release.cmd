4224a6c11b2fbb2cb8d382b7be504d9095be49b0
MMMZZZZ <m.zuidberg@icloud.com>
2020-08-09 21:22:01 +0200

    Automated Nextion Releases

--- Create_Release.cmd
+++ Create_Release.cmd
@@ -1,11 +1,23 @@
+rem Start Nextion Build VM.
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "Windows 7 Nextion VBox" poweroff
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" snapshot "Windows 7 Nextion VBox" restore "Starting in 3 seconds..." 
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm "Windows 7 Nextion VBox" --type headless
+rem Compile Tiva Firmware for all possible output numbers.
 for %%i in (1,2,3,4,5,6) do ( 
  C:\TI\ccsv8\eclipse\eclipsec -noSplash -data "C:\Users\Max\workspace_v8" -application com.ti.ccstudio.apps.projectBuild -ccs.projects Syntherrupter_Tiva -ccs.configuration Release_%%i
  robocopy Syntherrupter_Tiva\Release_%%i Syntherrupter_Firmwares Syntherrupter_Tiva.bin
  ren Syntherrupter_Firmwares\Syntherrupter_Tiva.bin Syntherrupter_Tiva_%%i_Coils.bin
  )
 ren Syntherrupter_Firmwares\Syntherrupter_Tiva_1_Coils.bin Syntherrupter_Tiva_1_Coil.bin
-robocopy Syntherrupter_Nextion Syntherrupter_Firmwares Syntherrupter_Nextion.HMI
+rem Build VM takes at least 20s to start, and at least 20s to compile one file. We assume that compiling the 6 Tiva firmwares take at least 20s. So... if no HMI file is there, the VM didn't start yet, or has already finished. 
 cd Syntherrupter_Firmwares
+if not exist "*.HMI" timeout /t 20
+rem Build VM is configured to delete temporary HMI files after creation. wait until no HMI files are left.
+:loop
+timeout /t 5
+if exist "*.HMI" goto loop
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "Windows 7 Nextion VBox" poweroff
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" snapshot "Windows 7 Nextion VBox" restore "Starting in 3 seconds..."
 "C:\Program Files\WinRAR\winrar" a -afzip -m5 ..\Syntherrupter_Firmwares.zip
 cd ..
 del /q Syntherrupter_Firmwares
