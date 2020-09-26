5e9eb7c2e237d815bef994c92effdc7262262e46
MMMZZZZ <m.zuidberg@icloud.com>
2020-09-27 00:20:27 +0200

    Fixed Typo and Nextion Text Script

--- Create_Nextion_Text.cmd
+++ Create_Nextion_Text.cmd
@@ -1,4 +1,4 @@
 rem https://github.com/MMMZZZZ/Nextion2Text
 
-del /q ":/Syntherrupter_Nextion/Source_as_Text/"
+del /q "./Syntherrupter_Nextion/Source_as_Text/"
 python "D:/Dokumente/GitHub/Nextion2Text/Nextion2Text.py" "./Syntherrupter_Nextion/" "Syntherrupter_Nextion.HMI" "Source_as_Text" ".txt"
